7e4f0febf3e4a381e7864d3352b1866a2d9382ac
NisCommits33 <Nixstha2@gmail.com>
2025-12-17 12:18:18 +0545

    feat: Initialize backend project with Poetry dependency management and core application files.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.v1 import api
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.api_router, prefix=settings.API_V1_STR)
 
